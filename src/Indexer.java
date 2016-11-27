8fcdc2190a3708d6c374e9600703a687191cafc6
Marco Morelos <Marco Morelos>
2016-11-26 18:01:17 -0800

    gets list of all documents

---
+++ src/Indexer.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.nio.file.*;
+
+//Write a program that uses the Lucene libraries to index all the html files in the folder you created in Part A
+//Handle different fields like title, body, creation date (if available).
+public class Indexer {
+
+    public static void main(String[] args) {
+        
+        
+        //prints error message if arguments are wrong then exits
+        if(args.length != 1){
+            System.out.println("Incorrect arguments passed. Arguments are of the form: \n[document storage path]");
+            return;
+        }
+        
+        ////print out the arguments
+        //for(String arg : args){
+        //    System.out.println(arg);
+        //}
+        
+        
+        //get document location
+        File[] docs = null;
+        try {
+            Path docPath = Paths.get(args[0]);
+            File docFolder = docPath.toFile();
+            docs = docFolder.listFiles();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        for(File f: docs){
+            System.out.println(f);
+        }
+        
+    }
+}
