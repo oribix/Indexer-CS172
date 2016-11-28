de49c7ec40ed5f06fec89890b5035b3834640fd4
Marco Morelos <Marco Morelos>
2016-11-27 19:53:10 -0800

    created Searcher. It takes in 2 arguments. The first argument is the index path. The second argument is the query. This is 80% complete. It still needs to be able to spit out the url that we need and possibly other things like page title.

---
+++ src/Searcher.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.nio.file.*;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.*;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.store.*;
+
+
+public class Searcher {
+    
+    public static void search(String indexDir, String q) throws IOException, ParseException{
+        
+        //creates an index searcher
+        Path indexPath = Paths.get(indexDir);
+        Directory dir = FSDirectory.open(indexPath);
+        IndexReader reader = DirectoryReader.open(dir);
+        IndexSearcher is = new IndexSearcher(reader);
+        
+        //Parses the query
+        QueryParser parser = new QueryParser("contents", new StandardAnalyzer());
+        Query query = parser.parse(q);
+        
+        //gets top 10 documents
+        TopDocs hits = is.search(query, 10);
+        
+        for(ScoreDoc scoreDoc : hits.scoreDocs){
+            Document doc = is.doc(scoreDoc.doc);
+            System.out.println(doc.get("fullpath"));
+        }
+        reader.close();
+    }
+    
+    public static void main(String[] args) {
+        
+        if(args.length < 2){
+            System.out.println("invalid arguments.\n\n"
+                    + "Arguments are of the form: \n"
+                    + "[index directory] <query>\n\n"
+                    + "Querys must be in quotes(\"\")");
+            return;
+        }
+        
+        for(String arg : args){
+            System.out.println(arg);
+        }
+        
+        try {
+            search(args[0], args[1]);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("\nDid you input a valid path for argument 0?");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+
+}
